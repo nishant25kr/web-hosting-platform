3694bd6e6da95087a0641395581795b8b0acbfae
nishant25kr <nishant927472@gmail.com>
2026-02-14 16:40:48 +0530

    added utils of uploding and getting all fils and cloning at local file

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,47 @@
-import express from "express"
+import express, { json } from "express"
 import cors from "cors"
-const app= express()
+import { generate } from "./utils/utils.js"
+import simpleGit from "simple-git"
+import path from "path"
+import { getAllFiles } from "./utils/utils.js"
+import { uploadeOnS3 } from "./utils/utils.js"
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express()
+app.use(express.json())
 app.use(cors())
 
 
+app.post("/deploye", async (req, res) => {
 
-app.post("/deploye",async(req,res)=>{
-
-    const url = req.body
-
-    
-
+    const { url } = req.body
+    const id = generate()
     console.log(url)
-    req.json({message: url})
+
+    await simpleGit().clone(url, path.join(__dirname, `output/${id}`))
+
+    const files = getAllFiles(path.join(__dirname, `output/0e3b3`));
+
+    files.forEach(async file => {
+        await uploadeOnS3(file.slice(__dirname.length + 1), file);
+
+    })
+
+    res.json({
+        id: id
+    })
+
+});
 
 
-
-})
 
 const PORT = 3000
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log("app is running")
 })
--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,6 +1,18 @@
+import fs from "fs"
+import path from "path"
+import { S3Client } from "@aws-sdk/client-s3";
+
 const MAX_LEN = 5
 
-function generate() {
+const s3 = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+export function generate() {
     let ans = ""
     const subset = "123456778990aldflaoqpueriunmcbzxcdafdf"
     for (let i = 0; i < MAX_LEN; i++) {
@@ -8,4 +20,36 @@ function generate() {
     }
     return ans;
 }
-// console.log(generate())
+
+export function getAllFiles(folderPath) {
+    let response = [];
+    const allFilesAndFolders = fs.readdirSync(folderPath);
+     
+    allFilesAndFolders.forEach(file => {
+        
+        const fullFilePath = path.join(folderPath, file);
+        
+        if (fs.statSync(fullFilePath).isDirectory()) {
+            response = response.concat(getAllFiles(fullFilePath))
+        } else {
+            response.push(fullFilePath);
+        }
+
+    });
+
+    return response;
+
+}
+
+export async function uploadeOnS3(fileName, localFilePath) {
+    const fileContent = fs.readFileSync(localFilePath)
+
+    const res = await s3.upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+    }).promise();
+    console.log(response);
+
+}
+
